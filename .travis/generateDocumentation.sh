3de7aae590604b387e0dc4c83e7fc8a025033e84
christ2go <christian@hagemeier.ch>
2017-03-29 16:38:55 +0200

    Fixed travis script file

--- .travis/generateDocumentation.sh
+++ .travis/generateDocumentation.sh
@@ -3,7 +3,7 @@
 shopt -s nullglob
 if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$CC" = "gcc" ] && ["$BUILD_TYPE" = "Release"] && ["$USE_GLBINDING" = "ON"]; then
   echo 'Setting up git'
-  pwd 
+  pwd
   git config --global push.default simple
   # Pretend to be an user called Travis CI.
   git config user.name "Travis CI Documentation Uploader"
@@ -17,17 +17,11 @@ if [ "$TRAVIS_OS_NAME" = "linux" ] && [ "$CC" = "gcc" ] && ["$BUILD_TYPE" = "Rel
   cp -r docs/doxygen/html SuperTux.github.io.git/docs
   ls SuperTux.github.io.git/docs/
   # Commit
+  cd SuperTux.github.io.git
   git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
 
-  # Force push to the remote gh-pages branch.
+  # Force push to the remote
   # The ouput is redirected to /dev/null to hide any sensitive credential data
   # that might otherwise be exposed.
   git push --force "https://${GH_REPO_TOKEN}@${GH_REPO_REF}" > /dev/null 2>&1
-
-  ##### Configure git.
-  # Set the push default to simple i.e. push only the current branch.
-  cd SuperTux.github.io.git
-  git add --all
-  git commit -m "Deploy code docs to GitHub Pages Travis build: ${TRAVIS_BUILD_NUMBER}" -m "Commit: ${TRAVIS_COMMIT}"
-  # Push to github
 fi
